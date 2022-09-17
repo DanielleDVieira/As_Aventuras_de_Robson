53deae33bc164629aac0eb6cff2ec21eb51876a5
Werner <guilhermeqwerner@gmail.com>
2022-09-17 18:09:19 -0300

    Pause menu

--- Assets/Scripts/Menu/menuScript.cs
+++ Assets/Scripts/Menu/menuScript.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class menuScript : MonoBehaviour
 {
 
-    public void loadScene(string scene){
+    public void loadScene(string scene)
+    {
         SceneManager.LoadScene(scene);
     }
 
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
