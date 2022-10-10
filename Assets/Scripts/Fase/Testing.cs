89ccf989c3fff6a3be39d90d5131d14a1b4909e0
Danielle <danielledvieira19@gmail.com>
2022-10-10 18:46:31 -0300

    Criação de arquivos txt contendo valores da grid

--- Assets/Scripts/Fase/Testing.cs
+++ Assets/Scripts/Fase/Testing.cs
@@ -20,19 +20,25 @@ public class Testing : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // Se clicado no botão esquerdo do mouse
         if (Input.GetMouseButtonDown(0)) {
             grid.SetValue(UtilsClass.GetMouseWorldPosition(), 1);
         }
 
+        // Se clicado no botão direito do mouse
         if (Input.GetMouseButtonDown(1)) {
             string gridParaArquivo = "";
 
+            // Formar texto com cada linha com as posições e os valores dos quadrados da grid
             for (int x = 0; x < 38; x++) {
                 for (int y = 0; y < 20; y++) {
                     gridParaArquivo += "SetValue("+x+", "+y+", "+grid.GetValue(x, y)+");\n";
                 }
             }
+            // salvar text após concatenação
             salvaGridArquivo(gridParaArquivo);
+
+            // Teste para salvar apenas uma linha no arquivo .txt e também debugar
             //int x, y;
             //grid.GetXY(UtilsClass.GetMouseWorldPosition(), out x, out y);
             //salvaGridArquivo("SetValue("+x+", "+y+", "+grid.GetValue(UtilsClass.GetMouseWorldPosition())+");");
@@ -40,8 +46,9 @@ public class Testing : MonoBehaviour
         }
     }
 
+    // Salvar text em arquivo .txt
     private void salvaGridArquivo(string text) {
-        string myfile = @"grid.txt";
+        string myfile = @"grid3.txt";
         if (!File.Exists(myfile)) {
             using(StreamWriter sw = File.AppendText(myfile)){
                 sw.WriteLine(text);
