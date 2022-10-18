f4a57a7165c2e7e6a09e2c6e1dec15c25d802ccc
joaoaugustoss <cop.joaoaugusto@gmail.com>
2022-10-18 17:09:54 -0300

    comenta

--- Assets/Scripts/Fase/TargetIndicator.cs
+++ Assets/Scripts/Fase/TargetIndicator.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -52,3 +52,4 @@ public class TargetIndicator : MonoBehaviour
     
 
 }
+*/
